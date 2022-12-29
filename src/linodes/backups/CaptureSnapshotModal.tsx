5e6af097499c373a87b614e2ca656197322c1d3f
Banks Nussman <banks@nussman.us>
2022-12-29 12:47:26 -0500

    use `react-hook-forms`

--- src/linodes/backups/CaptureSnapshotModal.tsx
+++ src/linodes/backups/CaptureSnapshotModal.tsx
@@ -1,5 +1,5 @@
 import { Button, FormControl, FormHelperText, FormLabel, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useToast } from "@chakra-ui/react";
-import { useState } from "react";
+import { useForm } from "react-hook-form";
 import { useCaptureSnapshotMutation } from "../../queries/linodes";
 
 interface Props {
@@ -10,15 +10,15 @@ interface Props {
 
 export function CaptureSnapshotModal({ isOpen, onClose, linodeId }: Props) {
   const { mutateAsync, isLoading } = useCaptureSnapshotMutation(linodeId);
-  const [label, setLabel] = useState<string>("");
+  const { register, handleSubmit, formState: { isDirty } } = useForm({ defaultValues: { label: "" } });
   const toast = useToast();
 
-  const onSubmit = () => {
+  const onSubmit = handleSubmit(({ label }) => {
     mutateAsync({ label }).then(() => {
       onClose();
       toast({ title: 'Sucessfully started snapshot capture.', status: "success" });
     });
-  };
+  });
 
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
@@ -29,10 +29,7 @@ export function CaptureSnapshotModal({ isOpen, onClose, linodeId }: Props) {
         <ModalBody>
           <FormControl>
             <FormLabel>Label</FormLabel>
-            <Input
-              value={label}
-              onChange={(e) => setLabel(e.target.value)}
-            />
+            <Input {...register('label')} />
             <FormHelperText>Provide a name for your snapshot</FormHelperText>
           </FormControl>
         </ModalBody>
@@ -41,7 +38,7 @@ export function CaptureSnapshotModal({ isOpen, onClose, linodeId }: Props) {
             colorScheme='blue'
             onClick={onSubmit}
             isLoading={isLoading}
-            isDisabled={!label}
+            isDisabled={!isDirty}
           >
             Capture
           </Button>
