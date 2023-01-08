e53691a9f595392191b61845d57909dceedeec31
Banks Nussman <banks@nussman.us>
2023-01-08 10:47:27 -0500

    added kubernetes create route

---
+++ src/kubernetes/Create.tsx
@@ -0,0 +1,13 @@
+import { Card, CardBody, Heading } from "@chakra-ui/react";
+
+export function Create() {
+  return (
+    <form>
+      <Heading size="lg" letterSpacing="tight" mb={4}>Create Cluster</Heading>
+      <Card variant="outline">
+        <CardBody>
+        </CardBody>
+      </Card>
+    </form>
+  );
+}
--- src/kubernetes/index.tsx
+++ src/kubernetes/index.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import { Cluster } from "./Cluster";
 import { Clusters } from "./Clusters";
+import { Create } from "./Create";
 
 export function KubernetesRouter() {
   return (
     <Routes>
+      <Route path="/create" element={<Create />} />
       <Route path="/:id" element={<Cluster />} />
       <Route path="/" element={<Clusters />} />
     </Routes>
